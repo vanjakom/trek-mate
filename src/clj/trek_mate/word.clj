1ecb33f2ad414ae419262a066d82d3be5203ee30
Vanja Komadinovic <vanjakom@gmail.com>
2020-08-16 00:22:37 +0200

    work on word support

--- src/clj/trek_mate/word.clj
+++ src/clj/trek_mate/word.clj
@@ -8,6 +8,11 @@
 ;; why 24, 16 + 8 ( from tile 256 x 256 )
 ;; projected points are between 0 and 2 ^ 24 ( 16 777 216 )
 
+;; A B C D
+;; E F G H
+;; I J K L
+;; M N O P
+
 (defn project-x [longitude]
   
   (long (Math/floor (* (+ (/ longitude 360) 0.5) (Math/pow 2 24)))))
@@ -36,46 +41,11 @@
              (+
               (if (zero? (bit-and y (bit-shift-left 1 upper))) 0 2)
               (if (zero? (bit-and y (bit-shift-left 1 lower))) 0 1))
-             2))]
+             4))]
        (str
         word
         (.toUpperCase (str (char (+ 97 index)))))))
    ""
    (partition 2 2 nil (reverse (range 0 24)))))
 
-(x->y->word (project-x 20.47302) (project-y 44.80425)) ;; "ECJCEAHEFDDD"
-
-(bit-and
- (project-x 20.45105)
- (bit-shift-left 1 2))
-
-(map #(str (char %)) (range 97 (+ 97 16)))
-
-(bit-and
- 11
- 4)
-
-(bit-and 235 199)
-
-(partition 2 2 nil (reverse (range 0 24)))
-
-(.toUpperCase (str (char 97)))
-
-(bit-shift-left 1 3)
-
-(project-x 20.45105) ;; 9341696 ;; 9341696
-
-(project-y 44.80815) ;; 6047807 ;; 5976708 ;; 6047807
-
-(project-y 44.80425)
-
-0.7047351352515348
-
-0.7046868414939647
-
-(Math/log 10)
-
-(Math/sin (/ (* 44.80425 Math/PI) 180))
-0.7046868414939647
-
-(project-y 44.80425)
+#_(x->y->word (project-x 21.02783) (project-y 44.80912))
